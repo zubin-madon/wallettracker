e617f8aead3410c5d1f519936a0378671d2e4b82
zubin-madon <madon.zubin@gmail.com>
2022-05-25 09:58:07 +0530

    initial repo to test netlify deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+  <MoralisProvider serverUrl={process.env.NEXT_APP_MORALIS_SERVER_URL} appId={process.env.NEXT_APP_MORALIS_APPLICATION_ID}>
+  <Component {...pageProps} />
+  </MoralisProvider>
+  )
 }
 
 export default MyApp
